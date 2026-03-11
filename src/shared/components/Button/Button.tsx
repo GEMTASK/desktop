7b0e312b8af710dc529ec1f0e3dce71bf3de8d7a
Mike Austin <mike_ekim@yahoo.com>
2026-03-11 08:19:06 -0700

    Update Button margin with only icon

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -76,7 +76,7 @@ function Button({
       {...props}
     >
       {icon && (
-        <Icon icon={icon} size={16} color={textColor} style={{ strokeWidth: 2.5, marginTop: -2, marginBottom: -2, marginLeft: children ? -3 : 0 }} />
+        <Icon icon={icon} size={16} color={textColor} style={{ strokeWidth: 2.5, marginTop: -2, marginBottom: -2, marginLeft: children ? -3 : -2, marginRight: -2 }} />
       )}
       {children && (
         <Text bold={bold} fontWeight={fontWeight} textColor={textColor}>
