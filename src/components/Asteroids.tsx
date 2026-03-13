a584ab57c9bc79b4e4e1ee3f1e53b286ecd36633
Mike Austin <mike_ekim@yahoo.com>
2026-03-13 08:56:20 -0700

    Add Asteroids velocity

--- src/components/Asteroids.tsx
+++ src/components/Asteroids.tsx
@@ -13,11 +13,15 @@ import ShipSvg from "./Ship.svg?react";
 function Asteroids() {
   const [state, setState] = useState({
     ship: {
-      position: {
-        x: 100,
-        y: 100
+      rotation: 0,
+      velocity: {
+        x: 0.0,
+        y: 0.0
       },
-      rotation: 0
+      position: {
+        x: 250,
+        y: 250
+      },
     }
   });
 
@@ -46,39 +50,48 @@ function Asteroids() {
     const timestampDelta = timestamp - lastTimestamp.current;
 
     if (keymapRef.current.ArrowRight) {
-      setState(state => ({
+      setState(({ ship }) => ({
         ship: {
-          ...state.ship,
-          rotation: state.ship.rotation + timestampDelta * 0.5
+          ...ship,
+          rotation: ship.rotation + timestampDelta * 0.25
         }
       }));
     }
 
     if (keymapRef.current.ArrowLeft) {
-      setState(state => ({
+      setState(({ ship }) => ({
         ship: {
-          ...state.ship,
-          rotation: state.ship.rotation - timestampDelta * 0.5
+          ...ship,
+          rotation: ship.rotation - timestampDelta * 0.25
         }
       }));
     }
 
     if (keymapRef.current.ArrowUp) {
-      setState(state => {
-        const ship = state.ship;
+      setState(({ ship }) => {
         const angle = (Math.PI / 180) * (ship.rotation - (90 + 45));
 
         return ({
           ship: {
-            ...state.ship,
-            position: {
-              x: ship.position.x + Math.cos(angle) - Math.sin(angle),
-              y: ship.position.y + Math.sin(angle) + Math.cos(angle),
-            }
+            ...ship,
+            velocity: {
+              x: ship.velocity.x + (Math.cos(angle) - Math.sin(angle)) * 0.05,
+              y: ship.velocity.y + (Math.sin(angle) + Math.cos(angle)) * 0.05,
+            },
           }
         });
       });
     }
+
+    setState(({ ship }) => ({
+      ship: {
+        ...ship,
+        position: {
+          x: ship.position.x + ship.velocity.x,
+          y: ship.position.y + ship.velocity.y,
+        }
+      }
+    }));
 
     lastTimestamp.current = timestamp;
 
@@ -93,8 +106,21 @@ function Asteroids() {
     };
   }, [handleAnimationFrame]);
 
+  // rotate(${state.ship.rotation}, 12.5, 15.25)
+
   return (
     <View as="svg" tabIndex={0} style={{ width: 500, height: 500 }} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp}>
+      {/* <svg
+        transform={`
+        translate(${state.ship.position.x}, ${state.ship.position.y})
+        rotate(${state.ship.rotation})
+      `}
+      >
+        <g transform="matrix(1, 0, 0, 1, -100, -100)">
+          <path d="M100,50 L50,180 L100,170 L150,180 L100,50Z" style={{ fill: "none", stroke: "black", strokeWidth: 10 }} />
+        </g>
+      </svg> */}
+
       <ShipSvg
         width={25}
         height={25}
