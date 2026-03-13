11fa496a432a8096ecc8c0e5b8f7cdfcb90c4ac3
Mike Austin <mike_ekim@yahoo.com>
2026-03-12 17:35:14 -0700

    Be able to move Asteroids ship

--- src/components/Asteroids.tsx
+++ src/components/Asteroids.tsx
@@ -13,6 +13,10 @@ import ShipSvg from "./Ship.svg?react";
 function Asteroids() {
   const [state, setState] = useState({
     ship: {
+      position: {
+        x: 100,
+        y: 100
+      },
       rotation: 0
     }
   });
@@ -44,15 +48,36 @@ function Asteroids() {
     if (keymapRef.current.ArrowRight) {
       setState(state => ({
         ship: {
+          ...state.ship,
           rotation: state.ship.rotation + timestampDelta * 0.5
         }
       }));
-    } else if (keymapRef.current.ArrowLeft) {
+    }
+
+    if (keymapRef.current.ArrowLeft) {
       setState(state => ({
         ship: {
+          ...state.ship,
           rotation: state.ship.rotation - timestampDelta * 0.5
         }
       }));
+    }
+
+    if (keymapRef.current.ArrowUp) {
+      setState(state => {
+        const ship = state.ship;
+        const angle = (Math.PI / 180) * (ship.rotation - (90 + 45));
+
+        return ({
+          ship: {
+            ...state.ship,
+            position: {
+              x: ship.position.x + Math.cos(angle) - Math.sin(angle),
+              y: ship.position.y + Math.sin(angle) + Math.cos(angle),
+            }
+          }
+        });
+      });
     }
 
     lastTimestamp.current = timestamp;
@@ -70,7 +95,14 @@ function Asteroids() {
 
   return (
     <View as="svg" tabIndex={0} style={{ width: 500, height: 500 }} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp}>
-      <ShipSvg width={25} height={25} transform={`rotate(${state.ship.rotation}, 12.5, 15.25)`} />
+      <ShipSvg
+        width={25}
+        height={25}
+        transform={`
+          translate(${state.ship.position.x}, ${state.ship.position.y})
+          rotate(${state.ship.rotation}, 12.5, 15.25)
+        `}
+      />
     </View>
   );
 }
