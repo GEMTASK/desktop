62dbfac6550827e6a13f31b3eb3697a652f4758e
Mike Austin <mike_ekim@yahoo.com>
2026-03-17 14:44:22 -0700

    Refactor Window resize logic

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -15,32 +15,106 @@ type PointerData = {
 // Frame
 //
 
+const ResizeVertical = {
+  NONE: 0,
+  TOP: 1,
+  BOTTOM: 3,
+} as const;
+
+const ResizeHorizontal = {
+  NONE: 0,
+  LEFT: 4,
+  RIGHT: 2,
+} as const;
+
+type ResizeState = [
+  typeof ResizeVertical[keyof typeof ResizeVertical],
+  typeof ResizeHorizontal[keyof typeof ResizeHorizontal]
+];
+
+const getResizeState = (x: number, y: number, width: number, height: number) => {
+  let vertical: ResizeState[0] = ResizeVertical.NONE;
+  let horizontal: ResizeState[1] = ResizeHorizontal.NONE;
+
+  switch (true) {
+    case y < 48:
+      vertical = ResizeVertical.TOP;
+      break;
+    case y > height - 48:
+      vertical = ResizeVertical.BOTTOM;
+      break;
+  }
+
+  switch (true) {
+    case x < 48:
+      horizontal = ResizeHorizontal.LEFT;
+      break;
+    case x > width - 48:
+      horizontal = ResizeHorizontal.RIGHT;
+      break;
+  }
+
+  return [vertical, horizontal] as const;
+};
+
+const getCursor = (vertical: ResizeState[0], horizontal: ResizeState[1]) => {
+  switch (true) {
+    case vertical === ResizeVertical.TOP && horizontal === ResizeHorizontal.LEFT
+      || vertical === ResizeVertical.BOTTOM && horizontal === ResizeHorizontal.RIGHT:
+      return "nwse-resize";
+    case vertical === ResizeVertical.TOP && horizontal === ResizeHorizontal.RIGHT
+      || vertical === ResizeVertical.BOTTOM && horizontal === ResizeHorizontal.LEFT:
+      return "nesw-resize";
+    case vertical === ResizeVertical.TOP || vertical === ResizeVertical.BOTTOM:
+      return "ns-resize";
+    case horizontal === ResizeHorizontal.LEFT || horizontal === ResizeHorizontal.RIGHT:
+      return "ew-resize";
+    default:
+      return "";
+  }
+};
+
 function Frame({ onUpdate }: { onUpdate: () => void; }) {
+  const dragState = useRef<ResizeState>([ResizeVertical.NONE, ResizeHorizontal.NONE]);
+
+  const handlePointerDown = (event: React.PointerEvent<HTMLElement>) => {
+    const clientRect = event.currentTarget.getBoundingClientRect();
+
+    const localX = event.clientX - clientRect.left;
+    const localY = event.clientY - clientRect.top;
+
+    const [vertical, horizontal] = getResizeState(localX, localY, clientRect.width, clientRect.height);
+
+    dragState.current = [vertical, horizontal];
+  };
+
+  const handlePointerUp = () => {
+    dragState.current = [ResizeVertical.NONE, ResizeHorizontal.NONE];
+  };
+
   const handlePointerMove = (event: React.PointerEvent<HTMLElement>) => {
     const clientRect = event.currentTarget.getBoundingClientRect();
 
     const localX = event.clientX - clientRect.left;
     const localY = event.clientY - clientRect.top;
 
-    if (localX < 48 && localY < 48) {
-      event.currentTarget.style.cursor = "nwse-resize";
-    } else if (localX > clientRect.width - 48 && localY < 48) {
-      event.currentTarget.style.cursor = "nesw-resize";
-    } else if (localX < 48 && localY > clientRect.height - 48) {
-      event.currentTarget.style.cursor = "nesw-resize";
-    } else if (localX > clientRect.width - 48 && localY > clientRect.height - 48) {
-      event.currentTarget.style.cursor = "nwse-resize";
-    } else if (localX >= 48 && localX <= clientRect.width - 48) {
-      event.currentTarget.style.cursor = "ns-resize";
-    } else if (localY >= 48 && localY <= clientRect.height - 48) {
-      event.currentTarget.style.cursor = "ew-resize";
-    } else {
-      event.currentTarget.style.cursor = "";
-    }
+    const [vertical, horizontal] = getResizeState(localX, localY, clientRect.width, clientRect.height);
+
+    event.currentTarget.style.cursor = getCursor(vertical, horizontal);
   };
 
   return (
-    <View absolute style={{ inset: -16, xcursor: "nw" }} onPointerMove={handlePointerMove} />
+    <View
+      absolute
+      style={{
+        inset: -16,
+        borderRadius: 8,
+        // background: "hsla(0, 0%, 0%, 0.1)"
+      }}
+      onPointerDown={handlePointerDown}
+      onPointerMove={handlePointerMove}
+      onPointerUp={handlePointerUp}
+    />
   );
 }
 
