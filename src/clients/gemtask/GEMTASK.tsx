e16962fa55ab335dcab9cf0fa4c1e908b76e09b1
Mike Austin <mike_ekim@yahoo.com>
2026-04-16 13:11:25 -0700

    Fix Sprint sticky position

--- src/clients/gemtask/GEMTASK.tsx
+++ src/clients/gemtask/GEMTASK.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 import { BookIcon, CalendarIcon } from "lucide-react";
 import { Avatar, Button, Divider, Icon, Select, Text, View, type Delegate } from "onyx-ui";
 
-import styles from "./GEMTASK.module.scss"
+import styles from "./GEMTASK.module.scss";
 
 function SectionHeader({
   children,
@@ -35,38 +37,51 @@ function Chip({
 }
 
 function Sprint() {
+  const [isBacklogVisible, setIsBacklogVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsBacklogVisible(isBacklogVisible => !isBacklogVisible);
+  };
+
   return (
-    <View border="top bottom" className={styles.Sprint}>
-      <View padding="8px 16px" spacing="8px" fillColor="content">
-        <Text>
+    <View>
+      <View sticky border="top bottom" padding="8px 16px" spacing="8px" fillColor="content" style={{ top: 35, zIndex: 1, margin: "-1px 0" }} className={styles.Sprint} onClick={handleClick}>
+        <Text bold>
           Sprint 1
         </Text>
         <Text light fontSize="12px">
           5 items
         </Text>
       </View>
+      {isBacklogVisible && (
+        <View border="top" padding="16px" fillColor="gutter">
+          <View border cornerRadius="4px" style={{ overflow: "hidden" }}>
+            <BacklogItem />
+          </View>
+        </View>
+      )}
     </View>
-  )
+  );
 }
 
 function BacklogItem() {
   return (
     <View horizontal padding="8px 16px" fillColor="content">
       <View flex spacing="8px">
-        <Text fontWeight="600">
+        <Text bold>
           An application can be filled out to become a Certified Scrum Trainer
         </Text>
         <View horizontal spacing="12px" align="middle left">
           <View horizontal spacing="4px" align="middle left">
             <Icon icon={BookIcon} size={14} style={{ marginTop: -4, marginBottom: -4 }} />
-            <Text light fontSize="12px" fontWeight="600">
+            <Text light bold fontSize="12px">
               ENG-2
             </Text>
           </View>
           <Chip light fontSize="12px" fillColor="icon">
             Basic Authentication
           </Chip>
-          <Chip light fontSize="12px" fontWeight="600" fillColor="highlight">
+          <Chip light bold fontSize="12px" fillColor="highlight">
             Apr 15
           </Chip>
         </View>
@@ -77,7 +92,7 @@ function BacklogItem() {
           <Avatar imageOnly />
         </Avatar.Stack>
         <View spacing="8px" align="middle center">
-          <Text fontWeight="600">
+          <Text bold>
             10
           </Text>
           <Text light fontSize="12px">
@@ -106,18 +121,19 @@ const sprints = [
   { title: "Sprint 1" },
   { title: "Sprint 2" },
   { title: "Sprint 3" },
-]
+];
 
 function GEMTASK() {
   return (
     <View flex style={{ minHeight: 0 }}>
+
       <View horizontal border="bottom" padding="8px 16px" align="middle justify" fillColor="white">
         <Text>
           GEMTASK
         </Text>
         <Avatar name="Sarah Connor" label="Quality Assurance" />
       </View>
-      <View border="bottom" padding="8px 16px" spacing="12px" fillColor="panel" style={{ marginBottom: -1, zIndex: 2 }}>
+      <View border="bottom" padding="8px 16px" spacing="16px" fillColor="panel" style={{ marginBottom: -1, zIndex: 2 }}>
         <View horizontal align="top justify" fillColor="panel">
           <View spacing="8px">
             <Text light fontSize="12px">
@@ -169,6 +185,7 @@ function GEMTASK() {
           The product backlog is a prioritized list of items with the most valuable at the top
         </Text>
       </View>
+
     </View>
   );
 }
