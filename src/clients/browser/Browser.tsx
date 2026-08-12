c36b1f4b3bc7320e3eaaef33138b2edf11215faa
Mike Austin <mike_ekim@yahoo.com>
2026-08-12 10:58:58 -0700

    Add ability to open / close left side panel

--- src/clients/browser/Browser.tsx
+++ src/clients/browser/Browser.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Button, Icon, Menu, Text, View } from "onyx-ui";
-import { BookmarkIcon, CalendarsIcon, HistoryIcon, HomeIcon, MenuIcon, PlusIcon } from "lucide-react";
+import { BookmarkIcon, CalendarsIcon, HistoryIcon, HomeIcon, MenuIcon, PlusIcon, PanelLeftCloseIcon, PanelLeftOpenIcon } from "lucide-react";
 
 import { Input } from "../terminal/Terminal";
 
@@ -10,6 +10,7 @@ function Browser({
   args?: Record<string, string>,
 }) {
   const [url, setUrl] = useState(args?.url ?? "https://gemtask.github.io");
+  const [isLeftPanelOpen, setIsLeftPanelOpen] = useState(false);
 
   const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
@@ -21,23 +22,28 @@ function Browser({
 
   return (
     <View flex horizontal>
-      <View border="right" style={{ width: 256 }}>
-        <View horizontal padding="8px" border="bottom" align="middle justify" fillColor="panel">
-          <Button hover icon={MenuIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
-          <View horizontal fillColor="panel">
-            <Button hover icon={CalendarsIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
-            <Button solid icon={BookmarkIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
-            <Button hover icon={HistoryIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+      {isLeftPanelOpen && (
+        <View border="right" style={{ width: 256 }}>
+          <View horizontal padding="8px" border="bottom" align="middle justify" fillColor="panel">
+            <Button hover icon={MenuIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+            <View horizontal fillColor="panel">
+              <Button hover icon={CalendarsIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+              <Button solid icon={BookmarkIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+              <Button hover icon={HistoryIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+            </View>
+            <Button hover icon={PlusIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
           </View>
-          <Button hover icon={PlusIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+          <View padding="8px 0px">
+            <Menu.Item icon={BookmarkIcon} title="GEMTASK – Agile, Simplified" />
+          </View>
         </View>
-        <View padding="8px 0px">
-          <Menu.Item icon={BookmarkIcon} title="GEMTASK – Agile, Simplified" />
-        </View>
-      </View>
+      )}
       <View flex>
         <View horizontal border="bottom" padding="8px" spacing="8px" align="middle left" fillColor="panel">
-          <Button hover icon={HomeIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+          <View horizontal>
+            <Button hover solid={isLeftPanelOpen} icon={isLeftPanelOpen ? PanelLeftCloseIcon : PanelLeftOpenIcon} style={{ minHeight: 32, padding: "8px 10px" }} onClick={() => setIsLeftPanelOpen(isLeftPanelOpen => !isLeftPanelOpen)} />
+            <Button hover icon={HomeIcon} style={{ minHeight: 32, padding: "8px 10px" }} />
+          </View>
           <Input flex border value={url} fillColor="content" cornerRadius="2px" style={{ minHeight: 32 }} onKeyDown={handleInputKeyDown} />
         </View>
         <View flex as="iframe" src={url} />
